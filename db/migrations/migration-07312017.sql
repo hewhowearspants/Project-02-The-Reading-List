6e3e0419e2381f57187df4799a9e9228917363c9
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-07-31 14:44:28 -0400

    created reading_list_dev database, tables for users, books

---
+++ db/migrations/migration-07312017.sql
@@ -0,0 +1,22 @@
+\c reading_list_dev
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_digest TEXT NOT NULL,
+    firstname VARCHAR(255),
+    lastname VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS books (
+    id SERIAL PRIMARY KEY,
+    book VARCHAR(255),
+    author VARCHAR(255),
+    year INTEGER,
+    genre VARCHAR(255),
+    read BOOLEAN,
+    user_id INTEGER REFERENCES users(id),
+    user_rating INTEGER,
+    user_notes TEXT
+);

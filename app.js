768cbc396507cfe0590c26c0debe482d48a7b047
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-08-04 17:32:19 -0400

    created rudimentary chat app...maybe...

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const session = require('express-session');
 const passport = require('passport');
 
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 require('dotenv').config();
 
@@ -24,24 +26,49 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+io.on('connection', function(socket) {
+  console.log('new connection from ' + socket.id);
+
+  socket.on('chat message', function(message) {
+    console.log('message: ' + message);
+    socket.emit('chat message', message);
+    socket.broadcast.emit('chat message', message);
+  });
+
+  socket.on('disconnect', function() {
+    console.log(socket.id + ' disconnected');
+
+  });
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`You're tuned in to port ${port}!`);
 });
 
+const authHelpers = require('./services/auth/auth-helpers');
+
+app.get('/chat', authHelpers.loginRequired, (req, res) => {
+  res.render('chat', {
+    message: 'ok',
+    user: req.user,
+    currentPage: 'chat',
+  });
+});
+
 app.get('/', (req, res) => {
-    res.render('index', {
-        message: '“We need to make books cool again. If you go home with somebody and they don\'t have books, don\'t fuck them.”',
-        currentPage: 'home',
-        documentTitle: 'The Reading List',
-        subTitle: '- John Waters',
-        user: req.user,
-    });
+  res.render('index', {
+      message: '“We need to make books cool again. If you go home with somebody and they don\'t have books, don\'t fuck them.”',
+      currentPage: 'home',
+      documentTitle: 'The Reading List',
+      subTitle: '- John Waters',
+      user: req.user,
+  });
 });
 
 const bookRoutes = require('./routes/book-routes');
